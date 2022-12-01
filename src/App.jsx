001a8f2390ba714632055293dbf548f05be2c65e
Jaimin Suthar <85049765+Jaimin78@users.noreply.github.com>
2022-12-01 05:00:46 +0000

    Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {Button} from 'react-bootstrap'
+export default function App() {
+  return (
+    <main>
+      <h1>E-Commerce Dashboard</h1>
+      <button>Normal</button>
+      <Button>Bootstrap</Button>
+    </main>
+  )
+}
